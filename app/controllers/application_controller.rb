53b69ffb6cce5dfd0c038904290a4705a6c34db5
Peter Petrov <pepetrov@gmail.com>
2009-12-13 16:30:28 +0100

    adding CACHE constant and cache method for all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,20 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-
+  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  # method to return a value from the cache corresponding to the passed key.
+  # If the value is not found, the method will do whatever you pass to it
+  # and will set the output of your passed block to the key you passed in the method.
+  # The object will expire in 1 hour. 
+  private def cache(key)
+    unless output = CACHE.get(key)
+      output = yield
+      CACHE.set(key, output, 1.hour)
+    end
+    return output
+  end
+  
 end
--- config/environment.rb
+++ config/environment.rb
@@ -38,4 +38,8 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  
+  # Memcached constant field
+  CACHE = MemCache.new('127.0.0.1')
+  
 end
